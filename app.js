694fc6e98e823a197bb991f187ca4c67b67c6362
Daniella Aceves <daniella.aceves98@gmail.com>
2021-05-24 13:03:43 -0700

    halfway through custom middleware

--- app.js
+++ app.js
@@ -8,6 +8,20 @@ const path = require('path');
 const jwt = require('jsonwebtoken');
 const SECRET = process.env.JWT;
 
+async function requireToken(req, res, next){
+  try {
+    const token = req.headers.authorization;
+    console.log('token--->', token)
+      const userInfo = await jwt.verify(token, SECRET);
+      console.log('userInfo--->', userInfo);
+      req.user = await User.byToken(userInfo.userId);
+      //console.log(req.user);
+      next()
+  } catch (error) {
+    next(error);
+  }
+}
+
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 
 app.post('/api/auth', async (req, res, next) => {
@@ -18,15 +32,32 @@ app.post('/api/auth', async (req, res, next) => {
   }
 });
 
-app.get('/api/auth', async (req, res, next) => {
+app.get('/api/auth', requireToken, async (req, res, next) => {
   try {
-    const token = req.headers.authorization;
-    const userInfo = await jwt.verify(token, SECRET);
-    res.send(await User.byToken(userInfo.userId));
+    requireToken(req, res, next);
+    console.log('inside get route---->', req.user)
+    res.send(req.user);
   } catch (ex) {
     next(ex);
   }
 });
+
+app.get('/api/users/:id/notes', async (req, res, next) => {
+  try {
+    //const token = req.headers.authorization;
+    //const userInfo = await jwt.verify(token, SECRET);
+    requireToken(req, res, next)
+    const user = await User.findByPk(req.params.id);
+    if(user.id === req.user.dataValues.id){
+      const notes = await user.getNotes()
+      res.send(notes);
+    } else{
+      res.status(401).send('something went wrong');
+    }
+  } catch (error) {
+    next(error);
+  }
+})
 
 app.use((err, req, res, next) => {
   console.log(err);
