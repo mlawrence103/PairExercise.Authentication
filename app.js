e9f064551b451ef993b36a81ca4b622e37a5c151
Daniella Aceves <daniella.aceves98@gmail.com>
2021-05-24 09:21:21 -0700

    initial commit added jwt functionality

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+const { models: { User }} = require('./db');
+const path = require('path');
+const jwt = require('jsonwebtoken');
+const SECRET = process.env.JWT;
+
+
+
+app.get('/', (req, res)=> res.sendFile(path.join(__dirname, 'index.html')));
+
+app.post('/api/auth', async(req, res, next)=> {
+  try {
+    res.send({ token: await User.authenticate(req.body)});
+  }
+  catch(ex){
+    next(ex);
+  }
+});
+
+app.get('/api/auth', async(req, res, next)=> {
+  try {
+    const token = req.headers.authorization;
+    const userInfo = await jwt.verify(token, SECRET);
+    res.send(await User.byToken(userInfo));
+  }
+  catch(ex){
+    next(ex);
+  }
+});
+
+app.use((err, req, res, next)=> {
+  console.log(err);
+  res.status(err.status || 500).send({ error: err.message });
+});
+
+module.exports = app;
